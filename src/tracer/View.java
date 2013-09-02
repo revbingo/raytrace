a642341d10a4b2b654d91bd16189f8db471ae6ba
Mark Piper <mark.piper@wds.co>
2013-09-02 21:00:39 +0100

    MP: Constatn

--- src/tracer/View.java
+++ src/tracer/View.java
@@ -2,7 +2,10 @@ package tracer;
 
 public class View {
 
+	private static final double ZOOM = 0.018;
+
 	public static final V3 VERTICAL_AXIS = new V3(0.0,0.0,1.0);
+	
 	public final V3 camera = new V3();
 	public final V3 lookAt = new V3();
 	public final V3 look = new V3();
@@ -10,13 +13,8 @@ public class View {
 	public final V3 vert = new V3();
 
 	public View() {
-		calibrateView();
-	}
-	
-	public void calibrateView() {
 		camera.set(2, -10, 7);
 		lookAt.set(0, 1, 0);
-
 		update();
 	}
 	
@@ -26,11 +24,10 @@ public class View {
 
 		horz.set(look.y, -look.x, 0);
 		vert.set(V3.cross(horz, look));
-
 		horz.norm();
 		vert.norm();
 
-		horz.mul(0.018);
-		vert.mul(0.018);
+		horz.mul(ZOOM);
+		vert.mul(ZOOM);
 	}
 }
