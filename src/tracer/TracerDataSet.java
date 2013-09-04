7e42f7214420cf5d87ba32710bb3d9e1d03e0a24
Mark Piper <mark.piper@wds.co>
2013-09-04 21:28:27 +0100

    MP: Simplify loop and remove extraneous variables:w

--- src/tracer/TracerDataSet.java
+++ src/tracer/TracerDataSet.java
@@ -18,18 +18,9 @@ import tracer.objects.SceneObject;
 public class TracerDataSet {
 	public final V3 currentRay = new V3();
 	public final V3 camera = new V3();
-	public final V3 tmpCurrentRay = new V3();
-	public final V3 tmpCamera = new V3();
-	public final V3 lineV = new V3();
 	public final V3 hit = new V3();
 
 	public int[] linepix;
 
 	public SceneObject bestObject;
-
-	final void updateLinepix(int width) {
-		if (linepix == null || linepix.length != width) {
-			linepix = new int[width];
-		}
-	}
 }
--- src/tracer/TracerThread.java
+++ src/tracer/TracerThread.java
@@ -21,6 +21,7 @@ import tracer.objects.SceneObject;
  * @author Hj. Malthaner
  */
 public class TracerThread extends Thread {
+	private static final long NO_COLOR = -1L;
 	private final TracerCoordinator coordinator;
 	private int yStart;
 	private int yEnd;
@@ -46,7 +47,7 @@ public class TracerThread extends Thread {
 		this.yEnd = yEnd;
 		this.halfWidth = width >> 1;
 		
-		tracerData.updateLinepix(width);
+		tracerData.linepix = new int[width];
 	}
 
 	@Override
@@ -60,14 +61,16 @@ public class TracerThread extends Thread {
 	}
 	
 	void calculateAndSetLinetracerData() {
+		V3 lineV = new V3();
+		
 		for (int y = yEnd; y > yStart; y--) {
 			//a ray through the scan line - set once and then added to each time below
-			tracerData.lineV.set(view.cameraToLookAt);
-			tracerData.lineV.add(view.vert, y);
+			lineV.set(view.cameraToLookAt);
+			lineV.add(view.vert, y);
 
 			for (int x = -halfWidth; x < halfWidth; x++) {
 				//the actual ray through the view
-				tracerData.currentRay.set(tracerData.lineV);
+				tracerData.currentRay.set(lineV);
 				tracerData.currentRay.add(view.horz, x);
 
 				tracerData.camera.set(view.camera);
@@ -81,28 +84,24 @@ public class TracerThread extends Thread {
 	}
 
 	private int traceObjects(TracerDataSet tracerData) {
-		boolean go;
 		int brightness = 255;
-
 		long objectRgb = -1;
 
-		do {
-			go = false;
-
+		while (brightness > 16) {
 			final double distanceToNearestObject = findIntersection(tracerData);
 
-			if (tracerData.bestObject != null) {
-				final long color = tracerData.bestObject.hit(tracerData.camera, tracerData.currentRay, scene.light, distanceToNearestObject);
+			if(tracerData.bestObject == null) break;
+			
+			final long color = tracerData.bestObject.hit(tracerData.camera, tracerData.currentRay, scene.light, distanceToNearestObject);
 
-				if (color == -1L) {
-					// mirror, p and v are set up by hit().
-					go = true;
-					brightness = brightness * 230 >> 8;
-				} else {
-					objectRgb = color;
-				}
+			if (color == NO_COLOR) {
+				// mirror, p and v are set up by hit().
+				brightness = brightness * 230 >> 8;
+			} else {
+				objectRgb = color;
+				break;
 			}
-		} while (go && brightness > 16);
+		}
 
 		if (objectRgb == -1) {
 			// Nothing hit
@@ -141,11 +140,11 @@ public class TracerThread extends Thread {
 
 		ArrayList<SceneObject> objects = scene.getSceneObjects();
 		for (int i = 0; i < objects.size(); i++) {
-			data.tmpCamera.set(data.camera);
-			data.tmpCurrentRay.set(data.currentRay);
+//			data.tmpCamera.set(data.camera);
+//			data.tmpCurrentRay.set(data.currentRay);
 
 			final SceneObject object = objects.get(i);
-			final double distanceToObject = object.distanceToIntersection(data.tmpCamera, data.tmpCurrentRay, raylen2);
+			final double distanceToObject = object.distanceToIntersection(data.camera, data.currentRay, raylen2);
 
 			if (distanceToObject >= 0 && distanceToObject < distanceToNearestObject) {
 				data.bestObject = object;
